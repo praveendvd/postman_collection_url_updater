ebf4b7a4c9cc4b2f326ae0ae4d59d632736cf6b2
praveendvd <praveendvd@live.com>
2022-02-01 18:53:56 +0000

    added

--- __test__/test-cli.system.test.js
+++ __test__/test-cli.system.test.js
@@ -5,42 +5,42 @@ const exec = require('shelljs').exec,
 describe('Validate index.js system test', () => {
   describe('Validate cli output messages', () => {
     it('Should show help if arguments are missing', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater" ', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater" ', { silent: true });
       expect(commandResponse.stderr).toMatchSnapshot();
     });
 
     it('Should show help if -c argument is missing', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -r "test" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -r "test" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
       expect(commandResponse.stderr).toMatchSnapshot();
       expect(commandResponse.stdout).toMatchSnapshot();
     });
 
     it('Should show help if -r argument is missing', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
       expect(commandResponse.stderr).toMatchSnapshot();
       expect(commandResponse.stdout).toMatchSnapshot();
     });
 
     it('Should show help if -w argument is missing', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -r "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -r "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
       expect(commandResponse.stderr).toMatchSnapshot();
       expect(commandResponse.stdout).toMatchSnapshot();
     });
 
     it('Should show file not found error if collection doesnt exists', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -c "test/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "new_collection.json"', { silent: true });
       expect(commandResponse.stderr).toMatchSnapshot();
       expect(commandResponse.stdout).toMatchSnapshot();
     });
 
     it('Should show file saved message correctly with default path', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -c "__test__/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -c "__test__/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}"', { silent: true });
       expect(commandResponse.stderr).toMatch('');
       expect(commandResponse.stdout).toMatch(/File saved to: .*[\\|\/]__test__[\\|\/]collection[\\|\/]new_collection.json/g);
     });
 
     it('Should show file saved message correctly with custom path', async () => {
-      const commandResponse = exec('".\\node_modules\\.bin\\postman-collection-url-updater"  -c "__test__/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "__test__/collection/output/new.json"', { silent: true });
+      const commandResponse = exec('"node_modules\\.bin\\postman-collection-url-updater"  -c "__test__/collection/collection.json" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "__test__/collection/output/new.json"', { silent: true });
       expect(commandResponse.stderr).toMatch('');
       expect(commandResponse.stdout).toMatch(/File saved to: .*[\\|\/]__test__[\\|\/]collection[\\|\/]output[\\|\/]new\.json/g);
     });
@@ -53,7 +53,7 @@ describe('Validate index.js system test', () => {
     it('validate no changes happens when there is no match', async () => {
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://localhost:23456/api/v1/{{path}}" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
       expect(outputCollection.item[0].item[0].request).toStrictEqual(sourceCollection.item[0].item[0].request);
       expect(outputCollection.item[1].item[0].item[0].item[0].item[0].item[0].item[0].item[0].request)
@@ -77,7 +77,7 @@ describe('Validate index.js system test', () => {
       }
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://www.testdomain0.ie" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://www.testdomain0.ie" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
 
       updatedRequestObject(sourceCollection.item[0].item[0], 1, 1);
@@ -105,7 +105,7 @@ describe('Validate index.js system test', () => {
       }
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "www.testdomain0.ie" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "www.testdomain0.ie" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
 
       updatedRequestObject(sourceCollection.item[0].item[0], 1, 1);
@@ -133,7 +133,7 @@ describe('Validate index.js system test', () => {
       }
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "www.testdomain0.ie/testpath/:pathvariable1-1" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "www.testdomain0.ie/testpath/:pathvariable1-1" -w "{{baseURL}}/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
 
       updatedRequestObject(sourceCollection.item[0].item[0], 1, 1);
@@ -164,7 +164,7 @@ describe('Validate index.js system test', () => {
       }
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "/:pathvariable1-1/path/request1-1?query1-1-0" -w "/:pathvariable1-1/path/request1-1?query1-1-0=queryvalue1-1-0-0&test" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "/:pathvariable1-1/path/request1-1?query1-1-0" -w "/:pathvariable1-1/path/request1-1?query1-1-0=queryvalue1-1-0-0&test" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
 
       updatedRequestObject(sourceCollection.item[0].item[0], 1, 1);
@@ -192,7 +192,7 @@ describe('Validate index.js system test', () => {
       }
 
       const sourceCollection = new SDK.Collection(JSON.parse(FS.readFileSync(collectionPath).toString())).toJSON();
-      const commandResponse = exec(`".\\node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://www.testdomain0.ie/testpath/:pathvariable1-1/path/request1-1?query1-1-0=queryvalue1-1-0&query1-1-1=queryvalue1-1-1" -w "{{protocol}}://{{baseURL}}/v1/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
+      const commandResponse = exec(`"node_modules\\.bin\\postman-collection-url-updater"  -c "${collectionPath}" -r "https://www.testdomain0.ie/testpath/:pathvariable1-1/path/request1-1?query1-1-0=queryvalue1-1-0&query1-1-1=queryvalue1-1-1" -w "{{protocol}}://{{baseURL}}/v1/{{path}}" -s "${outputCollectionPath}"`, { silent: true });
       const outputCollection = new SDK.Collection(JSON.parse(FS.readFileSync(outputCollectionPath).toString())).toJSON();
 
       updatedRequestObject(sourceCollection.item[0].item[0], 1, 1);
